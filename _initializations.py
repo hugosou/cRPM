f9ac05c06308d865d705c4f4c110b98e707739e3
hugosou <ucgthso@ucl.ac.uk>
2024-02-05 11:48:37 +0100

    squeeze

--- _initializations.py
+++ _initializations.py
@@ -218,7 +218,7 @@ class Mixin:
                     lengthscale,
                     fit_scale=fit_scale,
                     fit_lengthscale=fit_lengthscale
-                )
+                ).to(self.device.index)
             else:
                 raise NotImplementedError()
 
